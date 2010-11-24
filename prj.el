89c3ba23bfa8c91af42016812feed01b48fea9d0
David Ventimiglila <dventimi@gmail.com>
2010-11-23 20:36:53 -0800

    Initial import

---
+++ prj.el
@@ -0,0 +1,9 @@
+(jde-project-file-version "1.0")
+(jde-set-variables
+ '(jde-global-classpath (quote ("./classes" "./lib/junit-4.8.2.jar" "./lib/hsqldb.jar")))
+ '(jde-compiler (quote ("javac" "")))
+ '(jde-compile-option-directory "./classes")
+ '(jde-sourcepath (quote ("./src")))
+ '(jde-compile-option-debug (quote ("all" (t nil nil))))
+ '(jde-compile-option-hide-classpath t)
+ '(jde-compile-option-command-line-args (quote ("-Xlint:unchecked"))))
