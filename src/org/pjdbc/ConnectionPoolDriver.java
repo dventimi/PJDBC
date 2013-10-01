b3e966540e4a06ab5b89490ea384d56d6e9b258a
David A. Ventimiglia <dventimi@gmail.com>
2013-09-30 23:29:45 -0700

    Shortened.

--- src/org/pjdbc/ConnectionPoolDriver.java
+++ src/org/pjdbc/ConnectionPoolDriver.java
@@ -24,18 +24,15 @@ public class ConnectionPoolDriver extends AbstractProxyDriver {
 	Properties props = new Properties(properties);
 	props.setProperty("url", subName);
 	props.setProperty("index", "" + rand.nextInt(Integer.parseInt(size)));
-	Connection conn = pool.get(props);
-	if (conn==null) {
-	    conn = DriverManager.getConnection(subName, properties);
-	    pool.putIfAbsent(props, conn);}
-	return conn;}
+	if (pool.get(props)==null) pool.putIfAbsent(props, DriverManager.getConnection(subName, properties));
+	return pool.get(props);}
 
     public DriverPropertyInfo[] getPropertyInfo (String URL, Properties info) throws SQLException {
 	if (!acceptsURL(URL)) throw new SQLException("Invalid URL");
-	String[] parts = URL.split(":");
-	Driver driver = DriverManager.getDriver(parts[2]);
-	if (driver==null) throw new SQLException("No valid target driver registered for:  " + parts[2]);
-	return driver.getPropertyInfo(parts[2], info);}
+	String subName = URL.split(":")[2];
+	Driver driver = DriverManager.getDriver(subName);
+	if (driver==null) throw new SQLException("No valid target driver registered for:  " + subName);
+	return driver.getPropertyInfo(subName, info);}
 
     public int getMajorVersion () {return 1;}
 
