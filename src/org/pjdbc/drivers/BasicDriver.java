d7022c350abc64b4b7b2d3c29e58bf6c613be961
David A. Ventimiglia <dventimi@gmail.com>
2013-10-05 20:56:17 -0700

    Renamed

---
+++ src/org/pjdbc/drivers/BasicDriver.java
@@ -0,0 +1,20 @@
+package org.pjdbc.drivers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.pjdbc.lib.AbstractProxyDriver;
+
+public class BasicDriver extends AbstractProxyDriver {
+    private final int majorVersion = 1;
+    private final int minorVersion = 1;
+    private final boolean jdbcCompliant = false;
+
+    public boolean acceptsSubProtocol (String subprotocol) {
+	return "basic".equals(subprotocol);}
+
+    public Connection connect (String url, Properties info) throws SQLException {
+	if (!acceptsURL(url)) return null;
+	return DriverManager.getConnection(subname(url));}}
