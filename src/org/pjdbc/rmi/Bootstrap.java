6a6b071a85cd1babdc8e0afcd7d309def766c6f6
David A. Ventimiglia <dventimi@gmail.com>
2014-04-27 11:20:17 -0700

    Removed GenericProxyFactory

--- src/org/pjdbc/rmi/Bootstrap.java
+++ src/org/pjdbc/rmi/Bootstrap.java
@@ -1,5 +1,6 @@
 package org.pjdbc.rmi;
 
+import java.lang.reflect.*;
 import java.rmi.registry.*;
 import java.sql.*;
 import org.pjdbc.util.*;
@@ -8,5 +9,8 @@ public class Bootstrap {
     public static void main (String[] args) {
 	try {
 	    Class.forName(args[0]);
-	    LocateRegistry.getRegistry().bind(args[2], GenericProxyFactory.getProxy(DriverRemoteProxy.class, DriverManager.getDriver(args[1])));}
+	    final Driver targetDriver = DriverManager.getDriver(args[1]);
+	    LocateRegistry.getRegistry().bind(args[2], (DriverRemoteProxy)Proxy.newProxyInstance(Bootstrap.class.getClassLoader(), new Class[]{DriverRemoteProxy.class}, new InvocationHandler() {
+		    public Object invoke (Object proxy, Method method, Object[] args) throws SQLException {
+			try {return method.invoke(targetDriver, args);} catch (Exception e) {throw new SQLException(e);}}}));}
 	catch (Exception e) {e.printStackTrace(System.err);}}}
--- src/org/pjdbc/util/Pool.java
+++
@@ -1,3 +0,0 @@
-package org.pjdbc.util;
-
-public class Pool<K, V extends SmartMultiMap.Notifiable> extends SmartMultiMap<K, V> {}
