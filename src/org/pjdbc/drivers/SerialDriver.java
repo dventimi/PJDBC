8985d924744d184ab4d0790b10ad4f90a94a9184
David A. Ventimiglia <dventimi@gmail.com>
2014-05-06 19:54:50 -0700

    Fixed a typo in the SerialDriver

--- src/org/pjdbc/drivers/SerialDriver.java
+++ src/org/pjdbc/drivers/SerialDriver.java
@@ -5,7 +5,7 @@ import org.pjdbc.sql.*;
 import org.pjdbc.util.*;
 
 public class SerialDriver extends AbstractProxyDriver {
-    static {try {DriverManager.registerDriver(new SerialDriver());} serialch (Exception e) {throw new RuntimeException(e);}}
+    static {try {DriverManager.registerDriver(new SerialDriver());} catch (Exception e) {throw new RuntimeException(e);}}
 
     protected boolean acceptsSubProtocol (String subprotocol) {
 	return "serial".equals(subprotocol);}}
