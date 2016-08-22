a36e242c6f11c6e7f7c581c2db24d6a002753ff4
David A. Ventimiglia <dventimi@gmail.com>
2016-08-22 08:09:59 -0700

    Ported DriverComposition tests to JUnit

---
+++ src/test/java/LogDriver/DriverComposition.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.sql.*;
+import java.util.logging.*;
+import org.junit.*;
+import org.pjdbc.drivers.*;
+import static org.junit.Assert.*;
+
+public class DriverComposition {
+    @Test
+    public void progressiveComposition () {
+	try {
+	    assertNotNull(DriverManager.getConnection("jdbc:cat:jdbc:mock:foo"));
+	    assertNotNull(DriverManager.getConnection("jdbc:cat:jdbc:sink:jdbc:mock:foo"));
+	    assertNotNull(DriverManager.getConnection("jdbc:cat:jdbc:sink:jdbc:cat:jdbc:sink:jdbc:mock:foo"));
+	    assertNotNull(DriverManager.getConnection("jdbc:log:jdbc:mock:foo"));
+	    assertNotNull(DriverManager.getConnection("jdbc:filter:jdbc:mock:foo"));
+	    assertNotNull(DriverManager.getConnection("jdbc:log:jdbc:filter:jdbc:mock:foo"));
+	    assertNotNull(DriverManager.getConnection("jdbc:log:jdbc:filter:jdbc:cat:jdbc:sink:jdbc:mock:foo"));
+	    assertNotNull(DriverManager.getConnection("jdbc:log:jdbc:filter:jdbc:cat:jdbc:tee:jdbc:sink:jdbc:mock:foo;jdbc:mock:bar"));}
+	catch (Exception e) {fail(e.getMessage());}}
+
+    @Test
+    public void teePlusSink () {
+	try {
+	    Connection c = DriverManager.getConnection("jdbc:tee:jdbc:mock:foo;jdbc:sink:jdbc:mock:bar");
+	    MockDriver foo = (MockDriver)DriverManager.getDriver("jdbc:mock:foo");
+	    MockDriver bar = (MockDriver)DriverManager.getDriver("jdbc:mock:bar");
+	    assertSame(foo, bar);
+	    Statement stmt = c.createStatement();
+	    stmt.executeQuery("select * from person;");
+	    stmt.executeQuery("insert into person (last_name, first_name, age) values ('David', 'Ventimiglia', 42);");
+	    assertNotNull(foo.getLog("jdbc:mock:foo"));
+	    assertNotNull(bar.getLog("jdbc:mock:bar"));
+	    assertEquals("executeQuery[select * from person;]\n"+
+			 "executeQuery[insert into person (last_name, first_name, age) values ('David', 'Ventimiglia', 42);]",
+			 foo.getLog("jdbc:mock:foo"));
+	    assertEquals("", bar.getLog("jdbc:mock:bar"));}
+	catch (Exception e) {fail(e.getMessage());}}
+
+    @Test
+    public void teePlusSinkButReverseOrder () {
+	try {
+	    Connection c = DriverManager.getConnection("jdbc:tee:jdbc:sink:jdbc:mock:foo;jdbc:mock:bar");
+	    MockDriver foo = (MockDriver)DriverManager.getDriver("jdbc:mock:foo");
+	    MockDriver bar = (MockDriver)DriverManager.getDriver("jdbc:mock:bar");
+	    assertSame(foo, bar);
+	    Statement stmt = c.createStatement();
+	    stmt.executeQuery("select * from person;");
+	    stmt.executeQuery("insert into person (last_name, first_name, age) values ('David', 'Ventimiglia', 42);");
+	    assertNotNull(foo.getLog("jdbc:mock:foo"));
+	    assertNotNull(bar.getLog("jdbc:mock:bar"));
+	    assertEquals("", foo.getLog("jdbc:mock:foo"));
+	    assertEquals("executeQuery[select * from person;]\n"+
+			 "executeQuery[insert into person (last_name, first_name, age) values ('David', 'Ventimiglia', 42);]",
+			 bar.getLog("jdbc:mock:bar"));}
+	catch (Exception e) {fail(e.getMessage());}}
+
+    @Test
+    public void URLsCanHaveWhitespace () {
+	try {
+	    int i = 0;
+	    try{DriverManager.getConnection("jdbc: cat:jdbc:\nmock:foo");} catch (Exception e) {i++;}
+	    try{DriverManager.getConnection("jdbc: cat:jdbc:\nsink:jdbc:mock:foo");} catch (Exception e) {i++;}
+	    try{DriverManager.getConnection("jdbc: log:jdbc:\nfilter:jdbc:cat:jdbc:sink:jdbc:mock:foo");} catch (Exception e) {i++;}
+	    try{DriverManager.getConnection("jdbc: log:jdbc:\nfilter:jdbc\n:cat :  jdbc:\ntee:jdbc:sink :jdbc:mock:foo ;\njdbc:mock:bar");} catch (Exception e) {i++;}
+	    assert(i==4);}
+	catch (Exception e) {fail(e.getMessage());}}}
