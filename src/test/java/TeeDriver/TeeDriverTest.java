f7b93440cbb4021b421185cfeb5fdff23efc4efe
David A. Ventimiglia <dventimi@gmail.com>
2016-08-23 08:05:55 -0700

    Added TeeDriver tests

---
+++ src/test/java/TeeDriver/TeeDriverTest.java
@@ -0,0 +1,55 @@
+import java.sql.*;
+import org.junit.*;
+import org.pjdbc.drivers.*;
+import static org.junit.Assert.*;
+
+public class TeeDriverTest {
+
+    public void acceptsURL () {
+	assertFalse(new TeeDriver().acceptsURL("jdbc:tee"));
+	assertFalse(new TeeDriver().acceptsURL("jdbc:tee:"));
+	assertFalse(new TeeDriver().acceptsURL("jdbc:tee:foo"));
+	assertTrue(new TeeDriver().acceptsURL("jdbc:tee:jdbc:mock:foo;jdbc:mock:foo"));}
+
+    public void connectDirectlyAndInvokeMethods () {
+	try {
+	    Connection c = (Connection)(new TeeDriver().connect("jdbc:tee:jdbc:mock:foo;jdbc:mock:bar", null));
+	    MockDriver foo = (MockDriver)DriverManager.getDriver("jdbc:mock:foo");
+	    MockDriver bar = (MockDriver)DriverManager.getDriver("jdbc:mock:bar");
+	    assertSame(foo, bar);
+	    Statement stmt = c.createStatement();
+	    stmt.executeQuery("select * from person");
+	    stmt.executeQuery("insert into person (last_name, first_name, age) values ('David', 'Ventimiglia', 42)");
+	    assertNotNull(foo.getLog("jdbc:mock:foo"));
+	    assertEquals("executeQuery[select * from person]\n"+
+			 "executeQuery[insert into person (last_name, first_name, age) values ('David', 'Ventimiglia', 42)]",
+			 foo.getLog("jdbc:mock:foo"));
+	    assertNotNull(bar.getLog("jdbc:mock:bar"));
+	    assertEquals("executeQuery[select * from person]\n"+
+			 "executeQuery[insert into person (last_name, first_name, age) values ('David', 'Ventimiglia', 42)]",
+			 foo.getLog("jdbc:mock:bar"));}
+	catch (Exception e) {fail(e.getMessage());}}
+
+    public void connectDirectly () {
+	try {
+	    assertFalse(new TeeDriver().acceptsURL("foo"));
+	    assertNull(new TeeDriver().connect("foo", null));
+	    assertNull(new TeeDriver().connect("jdbc:tee", null));
+	    assertNull(new TeeDriver().connect("jdbc:tee:", null));
+	    assertNotNull(new TeeDriver().connect("jdbc:tee:jdbc:mock:foo;jdbc:mock:foo", null));}
+	catch (Exception e) {fail(e.getMessage());}}
+
+    public void connectIndirectlyWithBadURLFails () {
+	try{DriverManager.getConnection("jdbc:tee:jdbc:mock:foo");} catch (SQLException e) {return;}
+	fail("jdbc:tee:jdbc:mock:foo should have no registered driver!");}
+
+    public void connectIndirectlyWithGoodURLSucceeds () {
+	try {DriverManager.getConnection("jdbc:tee:jdbc:mock:foo;jdbc:mock:foo");}
+	catch (Exception e) {fail(e.getMessage());}}
+
+    public void compliance () {
+	assertFalse(new TeeDriver().jdbcCompliant());}
+
+    public void versionInfo () {
+	assertEquals(1, new TeeDriver().getMajorVersion());
+	assertEquals(0, new TeeDriver().getMinorVersion());}}
